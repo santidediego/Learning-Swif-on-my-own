0c174cfd9785ad8edf8308e229d9958873ff9e30
Santiago de Diego <santidediego@gmail.com>
2015-09-28 17:40:09 +0200

    Practicing loops

--- My first playground.playground/Contents.swift
+++ My first playground.playground/Contents.swift
@@ -16,5 +16,31 @@ str = str + " i´m fine"
 //If you want to use a variable you must write \()
 str = str + " \(number_five)"
 
+//Arrays
+var my_array = [1,2,3,4,5,6]
+my_array.count
+for var i=1;i<5;i++
+{
+    my_array.append(0)
+}
+my_array
+
+for index in 1...4
+{
+    my_array.removeLast()
+}
+my_array
+
+if my_array[4]==5
+{
+    str="The fourth element is \(my_array[4])"
+}
+
+while my_array[0] != 6
+{
+    my_array[0]++
+}
+my_array
+
 
 
